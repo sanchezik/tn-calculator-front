d29632c43f292138b5db23473318e6378b053e37
Alexander Tsetsersky <sanchezlife@tut.by>
2024-07-26 03:48:04 -0300

    edit vue js config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        allowedHosts: [
+            'localhost',
+            'ec2-15-229-250-69.sa-east-1.compute.amazonaws.com',
+            '192.168.1.1',
+            '127.0.0.1',
+        ],
+    },
 })
