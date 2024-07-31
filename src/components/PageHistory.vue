78d35c8c139987e255fe90a9fc0ab721e496162c
Alexander Tsetsersky <sanchezlife@tut.by>
2024-07-31 02:07:08 -0300

    History page - added deleting functionality

--- src/components/PageHistory.vue
+++ src/components/PageHistory.vue
@@ -40,12 +40,23 @@
         </tr>
         </thead>
         <tbody>
-        <tr v-for="(record, index) in filteredRecords" :key="index">
+        <tr v-for="(record, index) in filteredRecords" :key="index" :class="{ deleting: record.isDeleting }">
           <td>{{ record.date }}</td>
           <td>{{ record.amount }}</td>
           <td>{{ record.user_balance }}</td>
           <td>{{ record.operation.type }}</td>
           <td>{{ record.operation_response }}</td>
+          <td>
+            <b-button class="btn-danger" @click="deleteRecord(record)" :disabled=record.isDeleting>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash"
+                   viewBox="0 0 16 16" style="margin-top: -3px;">
+                <path
+                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                <path
+                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+              </svg>
+            </b-button>
+          </td>
         </tr>
         </tbody>
       </table>
@@ -137,6 +148,7 @@ export default {
             if (data["errors"]) {
               this.errors = data["errors"]
             } else {
+              data["records"].forEach(e => e.isDeleting = false);
               this.records = data["records"]
               this.pageSettings.number = data["pageNumber"]
               this.pageSettings.size = data["pageSize"]
@@ -155,7 +167,29 @@ export default {
         this.pageSettings.number = parseInt(page)
         this.getDataRequest()
       }
-    }
+    },
+    deleteRecord(record) {
+      record.isDeleting = true;
+      fetch(`http://ec2-15-229-250-69.sa-east-1.compute.amazonaws.com:5000/api/v1/my-records/${record.id}`, {
+        method: "DELETE",
+        credentials: "include",
+        headers: new Headers({'content-type': 'application/json'})
+      })
+          .then(response => response.json())
+          .then(response => {
+            console.log('Record deleted:', response);
+            this.records = this.records.filter(r => r.id !== record.id);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+    },
   },
 };
 </script>
+
+<style>
+.deleting {
+  background-color: lightgray;
+}
+</style>
